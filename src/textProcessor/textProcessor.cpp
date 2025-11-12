3975e5689b4ea14d8b8d0d474fbf61eae741f2aa
ArthurvMachado <avm.arthurv@gmail.com>
2025-11-12 15:36:08 -0300

    Add text loading and word breaking to TextProcessor
    Introduces methods to load text from a file and break it into words in the TextProcessor class. Updates processar to use the loaded text and word breaking functionality instead of stop words.

--- src/textProcessor/textProcessor.cpp
+++ src/textProcessor/textProcessor.cpp
@@ -4,6 +4,7 @@
 #include <string>
 #include <unordered_set>
 #include <fstream>
+#include <sstream>
 
 TextProcessor::TextProcessor() { stopWords.clear(); }
 TextProcessor::~TextProcessor() {}
@@ -20,11 +21,35 @@ bool TextProcessor::loadStopWords(const std::string& datapath){
     return true;
 }
 
+bool TextProcessor::loadText(const std::string& datapath){
+    std::ifstream text_a(datapath); // Carrega o texto desejado
+
+    if(!text_a) return false; // Arquivo não encontrado
+
+    std::stringstream buffer; // Buffer com o texto original
+    buffer << text_a.rdbuf(); // Lê todo o arquivo para o buffer
+
+    text = buffer.str();
+    return true;
+}
+
+std::vector<std::string> TextProcessor::breakWords(const std::string& text){
+    std::vector<std::string> words; // Vetor de palavras separadas
+    
+    std::istringstream buffer(text); // Buffer com o texto informado
+    
+    std::string str;
+    while(buffer >> str) words.push_back(str);
+
+    return words;
+}
+
 std::vector<std::string> TextProcessor::processar(std::string texto){
     std::vector<std::string> placeholder;
     
-    if(!loadStopWords("../stopWords.txt")) return placeholder;
+    if(!loadText("../machado/conto/contosFluminenses.txt")) return placeholder;
+    std::vector<std::string> words = breakWords(this->text);
 
-    for(std::string el : stopWords) std::cout << el << std::endl;
+    for(std::string el : words) std::cout << el << std::endl;
     return placeholder;
 }
--- src/textProcessor/textProcessor.hpp
+++ src/textProcessor/textProcessor.hpp
@@ -6,8 +6,12 @@
 class TextProcessor{
     private:
         std::unordered_set<std::string> stopWords;  // Stop words
+        std::string text;
 
-        bool loadStopWords(const std::string& datapath);
+        bool loadStopWords(const std::string& datapath); // Carrega as Stop Words
+        bool loadText(const std::string& datapath); // Carrega o texto de exemplo
+
+        std::vector<std::string> breakWords(const std::string& text); // Separa todas as palavras so texto
 
     public:
         TextProcessor(); // Construtor
