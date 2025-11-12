2b61893417f3ce3be63a350de5bc6ad645def0bc
Matt Batichotti <matebatichotti@gmail.com>
2025-11-12 14:25:44 -0300

    Descomentando algo

--- src/commandLineInterface/commandLineInterface.cpp
+++ src/commandLineInterface/commandLineInterface.cpp
@@ -2,8 +2,8 @@
 #include "../textProcessor/textProcessor.hpp"
 
 int main(int argc, char const *argv[]){
-    // TextProcessor processor;
-    // processor.processar(" ");
+    TextProcessor processor;
+    processor.processar(" ");
 
     return 0;
 }
